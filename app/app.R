5921a311786e2c2a03a1773f33706810f4e1a0e7
Charles Collins <coll5631@stthomas.edu>
2020-12-12 20:53:28 -0600

    added code for file upload

--- app/app.R
+++ app/app.R
@@ -1,4 +1,8 @@
 library(shiny)
+library(openxlsx)
+
+#allow for large excel file uploads up to 1 gb
+options(shiny.maxRequestSize = 1000 * 1024^2)
 
 # Define UI
 ui <- fluidPage(
@@ -9,18 +13,47 @@ ui <- fluidPage(
    # Sidebar
    sidebarLayout(
      sidebarPanel(
-       
+       #Import first file
+       fileInput(
+         inputId="file1",
+         label="First File: ",
+         accept = ".xlsx"
+       )
      )
       ,
       
       mainPanel(
+        tableOutput(outputId="file1_data")
       )
    )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
-   
+server <- function(input, output, session) {
+  
+  #Upload file1###################################
+  file1_data <- reactive({
+    req(input$file1)
+    
+    ext <- tools::file_ext(input$file1$name)
+    message(input$file$datapath)
+    switch(ext,
+           xlsx = openxlsx::read.xlsx(
+             xlsxFile=input$file1$datapath,
+             sheet=1,
+             startRow=1,
+             na.strings=""
+           ),
+           validate("Invalid file; Please upload a .xslx file")
+    )
+    
+  })
+  
+  output$file1_data <- renderTable({
+    file1_data()
+  })
+  ################################################
+  
 }
 
 # Run the application 
